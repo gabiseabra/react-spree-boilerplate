65c0b177ac6f54d84e547ed5161d12b565bc9629
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-06-02 17:21:01 -0300

    Added HomePage component

--- client/app/bundles/Spree/startup/client.js
+++ client/app/bundles/Spree/startup/client.js
@@ -1,10 +1,11 @@
 import ReactOnRails from "react-on-rails"
-import { Header, Footer } from "../components"
+import { Header, Footer, HomePage } from "../components"
 import { createStore, STORE_NAME } from "../redux"
 
 ReactOnRails.register({
   Header,
-  Footer
+  Footer,
+  HomePage
 })
 
 ReactOnRails.registerStore({ [STORE_NAME]: createStore })
