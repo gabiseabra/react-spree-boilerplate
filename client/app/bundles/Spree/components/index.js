1acbf39441eb41153815219896955cafe8ba1135
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-06-26 21:27:20 -0300

    Added Sidebar component

--- client/app/bundles/Spree/components/index.js
+++ client/app/bundles/Spree/components/index.js
@@ -1,3 +1,4 @@
 export { default as Shell } from "./Shell"
-export { default as HomePage } from "./HomePage"
+export { default as Sidebar } from "./Sidebar"
 export { default as Pagination } from "./Pagination"
+export { default as HomePage } from "./HomePage"
