48569a3cfe9a05c3b9681f7415cf0e06b4ff07c6
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-07-03 01:49:12 -0300

    Fixed ApiClient products endpoint

--- client/app/bundles/Spree/lib/ApiClient/endpoints/Pages/Products.js
+++ client/app/bundles/Spree/lib/ApiClient/endpoints/Pages/Products.js
@@ -3,16 +3,23 @@ import parseQuery from "./parseQuery"
 import { Product } from "../../models"
 
 export default class ProductsPage extends Endpoint {
-  Entity = Product
-
   routes() {
     return [
       { path: "/products", action: this.index },
-      { path: "/products/:id", action: this.show }
+      { path: "/products/:id", action: ({ params: { id } }) => this.show(id) }
     ]
   }
 
-  index = options => this.fetch(`/products?${parseQuery(options)}`, { collection: "products" })
+  index(options) {
+    return this.fetch(`/products?${parseQuery(options)}`, {
+      Entity: Product,
+      collection: "products"
+    })
+  }
 
-  show = ({ params: { id } }) => this.fetch(`/products/${id}`)
+  show({ params: { id } }) {
+    return this.fetch(`/products/${id}`, {
+      Entity: Product
+    })
+  }
 }
