8c2de41944311523f220763fe8916eaa92a97049
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-09-16 15:42:58 -0300

    ApiClient spec hooks

--- client/app/bundles/Spree/lib/ApiClient/spec/ApiClient.spec.js
+++ client/app/bundles/Spree/lib/ApiClient/spec/ApiClient.spec.js
@@ -3,86 +3,87 @@ import nock from "nock"
 import ApiClient, { CSRF_TOKEN_HEADER } from "../../ApiClient"
 
 describe("ApiClient", () => {
-  let scope
   const client = new ApiClient("http://test.api", {
     csrfToken: "csrf-token"
   })
+  beforeEach("setup nock scope", function () {
+    this.client = client
+    this.scope = nock("http://test.api")
+  })
+  afterEach("finish nock scope", function () {
+    this.scope.done()
+  })
 
   describe("#fetch()", () => {
-    beforeEach(() => {
-      scope = nock("http://test.api")
-      .get("/foo")
-      .reply(200)
+    beforeEach(function () {
+      this.scope
+        .get("/foo")
+        .reply(200)
     })
-    afterEach(() => scope.done())
 
-    it("accepts a path", async () => {
-      const response = await client.fetch("/foo")
+    it("accepts a path", async function () {
+      const response = await this.client.fetch("/foo")
       response.status.should.equal(200)
     })
 
-    it("accepts a request object", async () => {
-      const response = await client.fetch(new Request("/foo"))
+    it("accepts a request object", async function () {
+      const response = await this.client.fetch(new Request("/foo"))
       response.status.should.equal(200)
     })
   })
 
   describe("#json()", () => {
-    beforeEach(() => {
-      scope = nock("http://test.api")
+    beforeEach(function () {
+      this.scope
         .matchHeader("Accept", "application/json")
         .get("/json")
         .reply(200, { foo: 1, bar: 2 })
     })
-    afterEach(() => scope.done())
 
-    it("requests a json page and returns it's body", () => (
-      client.json("/json").should.eventually.deep.equal({ foo: 1, bar: 2 })
-    ))
+    it("requests a json page and returns it's body", function () {
+      return this.client.json("/json").should.eventually.deep.equal({ foo: 1, bar: 2 })
+    })
   })
 
   describe("#html()", () => {
-    beforeEach(() => {
-      scope = nock("http://test.api")
+    beforeEach(function () {
+      this.scope
         .matchHeader("Accept", "text/html")
         .get("/text")
         .reply(200, "<span>Test</span>")
     })
-    afterEach(() => scope.done())
 
-    it("requests an html page and returns it's body", () => (
-      client.html("/text").should.eventually.equal("<span>Test</span>")
-    ))
+    it("requests an html page and returns it's body", function () {
+      return this.client.html("/text").should.eventually.equal("<span>Test</span>")
+    })
   })
 
   describe("#text()", () => {
-    beforeEach(() => {
-      scope = nock("http://test.api")
+    beforeEach(function () {
+      this.scope
         .matchHeader("Accept", /^text\//)
         .get("/text")
         .reply(200, "Test")
     })
-    afterEach(() => scope.done())
 
-    it("requests a text page and returns it's body", () => (
-      client.text("/text").should.eventually.equal("Test")
-    ))
+    it("requests a text page and returns it's body", function () {
+      return this.client.text("/text").should.eventually.equal("Test")
+    })
   })
 
   describe("#refreshCsrfToken()", () => {
-    beforeEach(() => {
-      scope = nock("http://test.api")
+    beforeEach(function () {
+      this.scope
         .matchHeader(CSRF_TOKEN_HEADER, /^csrf-token/)
         .get("/authenticity_token")
         .reply(200, {
           authenticity_token: "csrf-token-test"
         })
     })
-    afterEach(() => scope.done())
 
-    it("updates client's CSRF token", async () => {
-      await client.refreshCsrfToken()
-      client.headers[CSRF_TOKEN_HEADER].should.equal("csrf-token-test")
+    it("updates client's CSRF token", async function () {
+      await this.client.refreshCsrfToken()
+      this.client.headers[CSRF_TOKEN_HEADER].should.equal("csrf-token-test")
     })
   })
 })
