f56a9fc704836630e04e4cc75689e5a686c40ac0
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-09-20 19:57:52 -0300

    Added status props to Response class

--- client/app/bundles/Spree/lib/ApiClient/Response.js
+++ client/app/bundles/Spree/lib/ApiClient/Response.js
@@ -4,6 +4,8 @@ import Collection from "./resources/Collection"
 export default class ApiResponse {
   constructor(response, data) {
     this.url = response.url
+    this.status = response.status
+    this.statusText = response.statusText
     this.data = data
     if(data instanceof Collection) {
       this.pagination = data.pagination
