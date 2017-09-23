9c084d8b6d1dea684101d9b6a01f7ded59a73161
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-09-23 16:24:45 -0300

    Added response resource getters

--- client/app/bundles/Spree/lib/ApiClient/Response.js
+++ client/app/bundles/Spree/lib/ApiClient/Response.js
@@ -1,5 +1,7 @@
-import Collection from "./resources/Collection"
-// import Resource from "./resources/Resource"
+import url from "url"
+import { Resource, Collection, Map } from "./resources"
+import { parse as pagination } from "./helpers/pagination"
+import { parse as search } from "./helpers/search"
 
 export default class ApiResponse {
   constructor(response, data) {
@@ -7,8 +9,35 @@ export default class ApiResponse {
     this.status = response.status
     this.statusText = response.statusText
     this.data = data
-    if(data instanceof Collection) {
-      this.pagination = data.pagination
+  }
+
+  get isResource() {
+    return (
+      this.data instanceof Resource ||
+      this.data instanceof Collection ||
+      this.data instanceof Map
+    )
+  }
+
+  get query() {
+    return url.parse(this.url, true).query
+  }
+
+  get search() {
+    return search(this.query)
+  }
+
+  get pagination() {
+    if(this.isResource) {
+      return this.data.pagination
     }
+    return pagination()
+  }
+
+  get collection() {
+    if(this.isResource) {
+      return this.data.collection
+    }
+    return undefined
   }
 }
