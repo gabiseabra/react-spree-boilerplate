59097b382e7d615fc57a1674dcd5354260191ca9
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-09-25 02:45:59 -0300

    Added ShowProductPage breadcrumbs

--- client/app/bundles/Spree/components/views/ShowProductPage/index.jsx
+++ client/app/bundles/Spree/components/views/ShowProductPage/index.jsx
@@ -3,15 +3,19 @@ import PropTypes from "prop-types"
 import { Info } from "../../product"
 import Loader from "../Loader"
 
-const ShowProductPage = ({ loading, product }) => (
-  <Loader loading={loading}>
-    {product && <Info product={product} />}
-  </Loader>
+const ShowProductPage = ({ loading, product, breadcrumbs }) => (
+  <div>
+    {breadcrumbs}
+    <Loader loading={loading}>
+      {product && <Info product={product} />}
+    </Loader>
+  </div>
 )
 
 ShowProductPage.propTypes = {
   loading: PropTypes.bool.isRequired,
-  product: PropTypes.object
+  product: PropTypes.object,
+  breadcrumbs: PropTypes.node
 }
 
 export default ShowProductPage
