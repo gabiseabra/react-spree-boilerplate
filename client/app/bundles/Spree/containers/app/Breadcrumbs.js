d19dc02e2c6048e13b944f2236a48194f3f1c64e
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-09-27 00:59:03 -0300

    Fixed Bradcrumb container

--- client/app/bundles/Spree/containers/app/Breadcrumbs.js
+++ client/app/bundles/Spree/containers/app/Breadcrumbs.js
@@ -1,10 +1,9 @@
 import { connect } from "react-redux"
 import { Breadcrumbs } from "../../components/app"
 import { getBreadcrumb } from "../../redux/selectors/taxonomies"
-import { loadPage } from "../../redux/modules/page"
 
 const props = (state, { taxonId }) => ({
   taxons: getBreadcrumb(state, { id: taxonId })
 })
 
-export default connect(props, { loadPage })(Breadcrumbs)
+export default connect(props)(Breadcrumbs)
