f5c28a80db839f2d412161b52b50ccaa9a917553
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-09-28 00:44:59 -0300

    cart_link

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -1,5 +1,11 @@
 Spree::StoreController.class_eval do
-  skip_before_action :set_current_order, only: :authenticity_token
+  skip_before_action :set_current_order, only: %i[cart_link authenticity_token]
+
+  def cart_link
+    order = simple_current_order
+    render partial: "spree/shared/order.json", locals: { order: order }
+    fresh_when order
+  end
 
   def authenticity_token
     render json: { authenticity_token: form_authenticity_token }
