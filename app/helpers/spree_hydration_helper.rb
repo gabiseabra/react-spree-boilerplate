467240703b9c645cf8c963e881f65ea425b404ed
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-09-27 21:34:53 -0300

    Fixed hydrate_order helper

--- app/helpers/spree_hydration_helper.rb
+++ app/helpers/spree_hydration_helper.rb
@@ -11,13 +11,13 @@ module SpreeHydrationHelper
     return unless spree_current_user
     json = render_to_json partial: 'spree/shared/user.json',
                           locals: { user: spree_current_user }
-    redux_store store_name, props: { user: json }
+    redux_store store_name, props: { current_order: json }
   end
 
   def hydrate_order(store_name)
-    return unless @order
+    return unless simple_current_order
     json = render_to_json partial: 'spree/shared/order.json',
-                          locals: { order: @order }
-    redux_store store_name, props: { order: order }
+                          locals: { order: simple_current_order }
+    redux_store store_name, props: { order: json }
   end
 end
