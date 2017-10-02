ac93819df798274e50817571ca50f2ee7ebf9525
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-10-02 20:33:32 -0300

    Fixed Spree undefined error

--- client/app/bundles/Spree/startup/client.js
+++ client/app/bundles/Spree/startup/client.js
@@ -2,6 +2,8 @@ import ReactOnRails from "react-on-rails"
 import * as containers from "../containers/views"
 import { createStore, STORE_NAME } from "../redux"
 
+window.Spree = {}
+
 ReactOnRails.register(containers)
 
 ReactOnRails.registerStore({ [STORE_NAME]: createStore })
