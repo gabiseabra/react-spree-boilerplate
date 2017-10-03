bf5f9df6dd08c21d17fdab10063c06f78b141ca4
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-10-02 22:36:28 -0300

    Catalog clearfix

--- client/app/bundles/Spree/components/product/Catalog/index.jsx
+++ client/app/bundles/Spree/components/product/Catalog/index.jsx
@@ -2,11 +2,12 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Row, Col } from "react-bootstrap"
 import Card from "../Card"
+import styles from "./Catalog.css"
 
 const Catalog = ({ products }) => (
-  <Row>
+  <Row className={styles.Catalog}>
     {products.map(p => (
-      <Col xs={12} sm={6} md={3} key={p.id}>
+      <Col xs={12} sm={6} md={4} key={p.id}>
         <Card product={p} />
       </Col>
     ))}
