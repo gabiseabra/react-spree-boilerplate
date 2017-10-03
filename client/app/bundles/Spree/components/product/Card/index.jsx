157fe3cc51fa3ff8834667e295f8dc4046212633
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-10-02 23:04:15 -0300

    Fixed product Card

--- client/app/bundles/Spree/components/product/Card/index.jsx
+++ client/app/bundles/Spree/components/product/Card/index.jsx
@@ -1,21 +1,20 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { Thumbnail, Button } from "react-bootstrap"
+import classnames from "classnames"
 import Price from "../Price"
 import Image from "./Image"
 import styles from "./Card.css"
 
 const ProductCard = ({ product }) => (
-  <Thumbnail className={styles.Card}>
-    <Image product={product.master} />
-    <div className={styles.body}>
-      <h3><a href={product.permalink}>{product.name}</a></h3>
+  <a href={product.permalink}>
+    <div className="thumbnail">
+      <Image product={product.master} />
+      <div className={classnames("caption", styles.body)}>
+        <h3><a href={product.permalink}>{product.name}</a></h3>
+        <Price product={product.master} />
+      </div>
     </div>
-    <div className={styles.controls}>
-      <Price product={product.master} />
-      <Button>Add to Cart</Button>
-    </div>
-  </Thumbnail>
+  </a>
 )
 
 ProductCard.propTypes = {
