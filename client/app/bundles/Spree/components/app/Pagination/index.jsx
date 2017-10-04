937fa781c93060f1b248c13e2a862b090d9bd5c1
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-10-04 00:06:48 -0300

    Centered pagination

--- client/app/bundles/Spree/components/app/Pagination/index.jsx
+++ client/app/bundles/Spree/components/app/Pagination/index.jsx
@@ -1,17 +1,20 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Pagination as PaginationComponent } from "react-bootstrap"
+import styles from "./Pagination.scss"
 
 const Pagination = ({ currentPage, totalPages, ...props }) => (
-  <PaginationComponent
-    prev next
-    first last
-    ellipsis
-    boundaryLinks
-    maxButtons={7}
-    items={totalPages}
-    activePage={currentPage}
-    {...props} />
+  <div className={styles.Pagination}>
+    <PaginationComponent
+      prev next
+      first last
+      ellipsis
+      boundaryLinks
+      maxButtons={7}
+      items={totalPages}
+      activePage={currentPage}
+      {...props} />
+  </div>
 )
 
 Pagination.propTypes = {
