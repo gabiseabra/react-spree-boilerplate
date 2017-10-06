580df35be650af46c95660c1823f834745282ae4
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-10-06 15:59:39 -0300

    Pagination component updates query string

--- client/app/bundles/Spree/containers/app/Pagination.jsx
+++ client/app/bundles/Spree/containers/app/Pagination.jsx
@@ -1,6 +1,10 @@
+import _ from "lodash"
+import qs from "querystring"
 import React, { Component } from "react"
 import PropTypes from "prop-types"
 import { connect } from "react-redux"
+import { withRouter } from "react-router"
+import { push } from "react-router-redux"
 import { Pagination } from "../../components/app"
 import { getPagination } from "../../redux/selectors/page"
 import { loadPage } from "../../redux/modules/page"
@@ -8,28 +12,52 @@ import { loadPage } from "../../redux/modules/page"
 class AppPagination extends Component {
   static propTypes = {
     pagination: PropTypes.object.isRequired,
-    loadPage: PropTypes.func.isRequired
+    location: PropTypes.object.isRequired,
+    loadPage: PropTypes.func.isRequired,
+    push: PropTypes.func.isRequired
   }
 
-  onSelect = page => this.props.loadPage(page)
+  onSelect = (page) => {
+    const { location } = this.props
+    const query = (
+      location.search ?
+      qs.parse(location.search.slice(1)) :
+      {}
+    )
+    this.props.loadPage(page)
+    this.props.push({
+      ...location,
+      search: qs.stringify({ ...query, page })
+    })
+  }
+
+  get restProps() {
+    return _.without(
+      this.props,
+      "pagination",
+      "loadPage",
+      "history",
+      "location",
+      "match",
+      "staticContext"
+    )
+  }
 
   render() {
-    const { pagination, ...props } = this.props
+    const { pagination } = this.props
     const { currentPage, totalPages } = pagination
-    delete props.loadPage
     return (
       <Pagination
         currentPage={currentPage}
         totalPages={totalPages}
         onSelect={this.onSelect}
-        {...props} />
+        {...this.restProps} />
     )
   }
 }
-
 
 const mapper = state => ({
   pagination: getPagination(state)
 })
 
-export default connect(mapper, { loadPage })(AppPagination)
+export default connect(mapper, { loadPage, push })(withRouter(AppPagination))
