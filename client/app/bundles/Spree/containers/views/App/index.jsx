a3b96088d0dada9efd3d6be5be3b7e4736e0e5f3
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-10-07 04:25:29 -0300

    Added base Helmet

--- client/app/bundles/Spree/containers/views/App/index.jsx
+++ client/app/bundles/Spree/containers/views/App/index.jsx
@@ -1,17 +1,23 @@
 import React from "react"
 import { Route, Switch } from "react-router-dom"
+import { Helmet } from "react-helmet"
 import HomePage from "./HomePage"
 import ProductsPage from "./ProductsPage"
 import ShowProductPage from "./ShowProductPage"
 import ShowTaxonPage from "./ShowTaxonPage"
 
 const App = () => (
-  <Switch>
-    <Route exact path="/" component={HomePage} />
-    <Route exact path="/products" component={ProductsPage} />
-    <Route exact path="/products/:id" component={ShowProductPage} />
-    <Route exact path="/t/:id*" component={ShowTaxonPage} />
-  </Switch>
+  <div>
+    <Helmet
+      defaultTitle="React Spree Boilerplate"
+      titleTemplate="%s - React Spree Boilerplate" />
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/products" component={ProductsPage} />
+      <Route exact path="/products/:id" component={ShowProductPage} />
+      <Route exact path="/t/:id*" component={ShowTaxonPage} />
+    </Switch>
+  </div>
 )
 
 export default App
