8b2c1f23da9e0616fab3f0665a33a990593dafbe
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-10-12 02:48:43 -0300

    Added Page container load prop

--- client/app/bundles/Spree/containers/page/Page/index.jsx
+++ client/app/bundles/Spree/containers/page/Page/index.jsx
@@ -5,40 +5,54 @@ import PropTypes from "prop-types"
 import { withRouter } from "react-router-dom"
 import { connect } from "react-redux"
 import { Page } from "../../../components/page"
-import { load, loadPage } from "../../../redux/modules/page"
+import { load as loadPath, loadPage } from "../../../redux/modules/page"
 import Content from "./Content"
 
 const parseQuery = search => (search ? qs.parse(search.slice(1)) : {})
 
+const getPath = props => (
+  typeof props.load === "string" ?
+  props.load :
+  props.load && props.location.pathname
+)
+
 class PageApp extends Component {
   static propTypes = {
+    // eslint-disable-next-line react/no-unused-prop-types
+    load: PropTypes.oneOfType(
+      PropTypes.bool,
+      PropTypes.string
+    ).isRequired,
     children: PropTypes.node.isRequired,
     location: PropTypes.object.isRequired,
     loadPage: PropTypes.func.isRequired,
-    load: PropTypes.func.isRequired
+    loadPath: PropTypes.func.isRequired
+  }
+
+  static defaultProps = {
+    load: false
   }
 
   componentWillMount() {
-    this.load(this.props)
+    if(this.props.load) this.loadPath(this.props)
   }
 
   componentWillReceiveProps(next) {
-    const location = this.props.location
-    const query = parseQuery(location.search)
-    const nextLocation = next.location
-    const nextQuery = parseQuery(nextLocation.search)
-    if(nextLocation.pathname !== location.pathname ||
+    if(!next.load) return
+    const query = parseQuery(this.props.location.search)
+    const nextQuery = parseQuery(next.location.search)
+    if(getPath(next) !== getPath(this.props) ||
       !_.isEqual(nextQuery.search, query.search)) {
-      this.load(next)
+      this.loadPath(next)
     } else if(nextQuery.page !== query.page) {
       this.loadPage(next)
     }
   }
 
-  load({ location }) {
-    const query = parseQuery(location.search)
-    this.props.load(
-      location.pathname,
+  loadPath(props) {
+    const query = parseQuery(props.location.search)
+    this.props.loadPath(
+      getPath(props),
       query.page || 1,
       query.search
     )
@@ -49,20 +63,9 @@ class PageApp extends Component {
     this.props.loadPage(query.page || 1)
   }
 
-  get restProps() {
-    return _.without(
-      this.props,
-      "load",
-      "history",
-      "location",
-      "match",
-      "staticContext"
-    )
-  }
-
   render() {
     const { children } = this.props
-    return <Page {...this.restProps}>{children}</Page>
+    return <Page>{children}</Page>
   }
 }
 
@@ -70,4 +73,4 @@ PageApp.Title = Page.Title
 
 PageApp.Content = Content
 
-export default connect(undefined, { load, loadPage })(withRouter(PageApp))
+export default connect(undefined, { loadPath, loadPage })(withRouter(PageApp))
