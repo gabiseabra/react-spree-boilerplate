180f9dcd8f5713e63f3879c4d5931effd7a3ca33
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-10-13 15:57:38 -0300

    Added Order resource checkout data

--- client/app/bundles/Spree/lib/ApiClient/resources/entities/Order.js
+++ client/app/bundles/Spree/lib/ApiClient/resources/entities/Order.js
@@ -1,4 +1,5 @@
 import LineItem from "./LineItem"
+import Address from "./Address"
 import Resource from "../Resource"
 
 const STEPS = [
@@ -16,12 +17,15 @@ export default class Order extends Resource {
 
   constructor(data) {
     super()
+    const shipping = data.ship_address
+    const billing = data.bill_address
     this.id = data.id
     this.number = data.number
     this.token = data.token
     this.state = data.state
     this.userId = data.user_id
     this.quantity = data.quantity
+    this.lineItems = data.line_items.map(item => new LineItem(item))
     this.price = {
       total: parseFloat(data.total),
       items: parseFloat(data.item_total),
@@ -29,7 +33,11 @@ export default class Order extends Resource {
       shipping: parseFloat(data.ship_total),
       adjustment: parseFloat(data.adjustment_total)
     }
-    this.lineItems = data.line_items.map(item => new LineItem(item))
+    this.addresses = {
+      shipping: (shipping ? new Address(shipping) : undefined),
+      billing: (billing ? new Address(billing) : undefined)
+    }
+    this.steps = data.checkout_steps
   }
 
   static hydrate({ order }) {
