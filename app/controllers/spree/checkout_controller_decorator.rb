bff47ced7b9ffb1980c1c25a4c990d2641000860
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-10-14 09:59:03 -0300

    Removed checkout controller hydration

--- app/controllers/spree/checkout_controller_decorator.rb
+++
@@ -1,8 +0,0 @@
-Spree::CheckoutController.class_eval do
-  include ReduxStoreHydration
-  respond_to :html, :json
-  hydrate 'spreeStore',
-          :collections,
-          only: :edit
-  collection :countries, of: -> { Spree::Country.all }
-end
