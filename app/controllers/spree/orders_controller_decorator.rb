c7c8b74a8b1cc37b65f8c83802392776cac375d9
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-10-17 21:29:41 -0200

    Fixed default orders controller layout

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,4 +1,14 @@
 Spree::OrdersController.class_eval do
-  layout 'react_spree_application'
+  layout :layout
   respond_to :html, :json
+
+  private
+
+  def layout
+    if action_name == 'edit'
+      'react_spree_application'
+    else
+      'spree_application'
+    end
+  end
 end
