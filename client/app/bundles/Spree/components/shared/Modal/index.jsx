15f5c00bf3c4a7a4a4e339df1c838d88db9935c5
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-11-15 16:47:44 -0200

    Updated react modules

--- client/app/bundles/Spree/components/shared/Modal/index.jsx
+++ client/app/bundles/Spree/components/shared/Modal/index.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react"
 import PropTypes from "prop-types"
 import { Modal } from "react-bootstrap"
-import "./overlayFix"
 
 class ControlledModal extends Component {
   static propTypes = {
